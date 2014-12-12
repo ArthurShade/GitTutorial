e46ba35642c659f2a5b0eb0c77868d11a52941b3
Phillip Smyth <last5@hotmail.co.uk>
2014-12-12 08:41:29 +0000

    empty constructor

--- My Game/Game.cpp
+++ My Game/Game.cpp
@@ -1,11 +1,7 @@
 #include "Game.h"
 
-
-<<<<<<< HEAD:My Game/Game.cpp
 Game::Game(){}
-=======
-Game::Game(){int i = 0; int a = 2;}
->>>>>>> 0b6ec05ff2abca02f608a5288a7a31317af06ef6:My Game/My Game/Game.cpp
+
 
 
 Game::~Game(){}
